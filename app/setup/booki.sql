9c3c71181705ccf7f7a794b6f25aca7888cbd669
Duy Nguyen <hoanduy27@gmail.com>
2021-06-02 12:18:04 +0700

    add procedures for reviews and carts

--- app/setup/booki.sql
+++ app/setup/booki.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 01, 2021 at 08:48 PM
+-- Generation Time: Jun 02, 2021 at 07:17 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -25,6 +25,41 @@ DELIMITER $$
 --
 -- Procedures
 --
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ADD_BOOK` (IN `_name` TEXT, IN `_author` TEXT, IN `_publisher` TEXT, IN `_description` TEXT, IN `_image` TEXT, IN `_amount` INT, IN `_price` INT)  BEGIN 
+	INSERT INTO `book` (name, description, author, publisher, image, amount, price)
+    VALUES(
+        _name,
+        _description,
+        _author,
+        _publisher,
+        _image,
+        _amount,
+        _price
+    );
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ADD_REVIEW` (IN `_uid` INT, IN `_bid` INT, IN `_rating` INT, IN `_comment` TEXT)  BEGIN
+    INSERT INTO `review` (uid, bid, rating, comment)
+    VALUES (_uid, _bid, _rating, _comment);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ADD_TO_CART` (IN `_uid` INT, IN `_bid` INT)  BEGIN 
+    IF(EXISTS(
+        SELECT uid 
+        FROM `cart`
+        WHERE uid = _uid AND bid = _bid
+    )) THEN
+        UPDATE `cart`
+        SET amount = amount + 1
+        WHERE uid = _uid AND bid = _bid;
+
+    ELSE 
+        INSERT INTO `cart`(uid, bid, amount)
+        VALUES(_uid, _bid, 1);
+    END IF;
+	
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `CHECK_EMAIL` (IN `_email` TEXT COLLATE utf8mb4_unicode_ci)  BEGIN
     DECLARE result BOOLEAN;
     IF(EXISTS(
@@ -58,9 +93,29 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `CREATE_USER` (IN `uname` VARCHAR(15
     VALUES (uname, PASSWORD(psswd), name, phone, email, 'customer');
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GET_ALL_BOOKS` ()  BEGIN 
+	SELECT name, author, publisher, amount, description, price
+    FROM `book`;
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GET_ALL_USERS` ()  BEGIN 
 	SELECT name, username, phone, email, role, active
     FROM `user` ;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GET_BOOK` (IN `id` INT)  BEGIN 
+	SELECT name, author, publisher, amount, description, price
+    FROM `book` 
+    WHERE bid = id; 
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GET_BOOK_REVIEWS` (IN `_bid` INT)  BEGIN 
+	SELECT user.name, rating, comment
+    FROM user, book, review
+    WHERE
+        review.bid = _bid
+        AND book.bid = review.bid
+        AND user.uid = review.uid;
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GET_USER_BY_ID` (IN `id` INT)  BEGIN 
@@ -75,15 +130,52 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `GET_USER_BY_USN` (IN `usn` VARCHAR(
     WHERE username = usn; 
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `UPDATE_INFO` (IN `uname` VARCHAR(15) COLLATE utf8mb4_unicode_ci, IN `psswd` VARCHAR(30), IN `_name` VARCHAR(50) COLLATE utf8mb4_unicode_ci, IN `_phone` VARCHAR(10) COLLATE utf8mb4_unicode_ci, IN `_email` TEXT COLLATE utf8mb4_unicode_ci)  BEGIN
-    UPDATE `user`
-    SET
-    	password = PASSWORD(psswd),
-        name = _name,
-        phone = _phone,
-        email = _email
+CREATE DEFINER=`root`@`localhost` PROCEDURE `REMOVE_BOOK` (IN `_bid` INT)  BEGIN 
+	DELETE FROM `book`
+    WHERE bid = _bid;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `REMOVE_CART_ITEM` (IN `_uid` INT, IN `_bid` INT)  BEGIN 
+    DELETE FROM `cart`
+    WHERE uid = _uid AND bid = _bid;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SHOW_CART_ITEMS` (IN `_uid` INT)  BEGIN 
+	SELECT book.name, cart.amount, book.price, cart.amount*book.price as total_price
+    FROM `book`, `user`, `cart`
     WHERE
-    	username = uname;
+        cart.uid = _uid 
+        AND cart.bid = book.bid
+        AND cart.uid = user.uid;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UPDATE_BOOK_INFO` (IN `id` INT, IN `_name` TEXT COLLATE utf8mb4_unicode_ci, IN `_author` TEXT COLLATE utf8mb4_unicode_ci, IN `_publisher` TEXT COLLATE utf8mb4_unicode_ci, IN `_description` TEXT COLLATE utf8mb4_unicode_ci, IN `_image` TEXT COLLATE utf8mb4_unicode_ci, IN `_amount` INT, IN `_price` INT)  BEGIN 
+	UPDATE `book`
+    SET 
+        name = _name,
+        author = _author,
+        publisher = _publisher,
+        description = _description,
+        image = _image,
+        amount = _amount,
+        price = _price
+
+    WHERE bid = id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UPDATE_CART_ITEM` (IN `_uid` INT, IN `_bid` INT, IN `_amount` INT)  BEGIN 
+    UPDATE `cart`
+    SET amount = _amount
+    WHERE uid = _uid AND bid = _bid;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UPDATE_INFO` (IN `uname` VARCHAR(15) COLLATE utf8mb4_unicode_ci, IN `psswd` VARCHAR(30), IN `_name` VARCHAR(50) COLLATE utf8mb4_unicode_ci, IN `_phone` VARCHAR(10) COLLATE utf8mb4_unicode_ci, IN `_email` TEXT COLLATE utf8mb4_unicode_ci)  BEGIN 
+    UPDATE `user` 
+    SET password = PASSWORD(psswd), 
+        name = _name, 
+        phone = _phone, 
+        email = _email 
+    WHERE username = uname; 
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `VALIDATE_LOGIN` (IN `usn` VARCHAR(15) COLLATE utf8mb4_unicode_ci, IN `psswd` VARCHAR(30) COLLATE utf8mb4_unicode_ci, IN `_role` VARCHAR(9) COLLATE utf8mb4_unicode_ci)  BEGIN
@@ -120,8 +212,18 @@ CREATE TABLE `book` (
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `amount` int(11) NOT NULL,
   `price` int(11) NOT NULL,
-  `image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `author` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publisher` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`bid`, `name`, `description`, `amount`, `price`, `image`, `author`, `publisher`) VALUES
+(1, 'Black Clover - Tập 22: Hừng Đông', 'Cùng với Lemiel - Ma pháp đế đầu tiên và tộc trưởng Licht của tộc Elf, một vài Elf và các ma pháp kị sĩ đã cùng nhau chống lại ác ma trong cuộc tổng công kích vượt không gian.  Trước sức mạnh khó lường của ác ma, số phận của cuộc chiến rơi vào tay Asta và Yuno bởi hai người không ngừng tiến bộ khi trận đấu ngày một khốc liệt. Mong sao họ có thể chấm dứt nỗi đau khổ và hận thù kéo dài hàng trăm năm!!\r\n\r\nGood faith” “hope” “love”is hidden in the leaf of the clover each.\r\n\r\n“Good luck” dwells in the fourth leaf.\r\n\r\n“The Devil” lives in the fifth leaf.\r\n\r\nYuki Tabata\r\n\r\n“Tôi có con rồiii!!! Không ngờ một người như tôi cũng có ngày được làm cha…  Chân thành cảm ơn người vợ đã cố gắng hết mình và cô con gái vừa chào đời của tôi!!”', 100, 22500, NULL, 'Yuki Tanaba', 'NXB Kim Đồng'),
+(3, 'Yu-Gi-Oh! - Tập 37', 'Cách đây đúng 16 năm, Yu-Gi-Oh! hay còn được biết đến với tên Vua Trò Chơi là bộ truyện tranh đã tạo nên \"hiện tượng xuất bản\" tại Nhật và Việt Nam, thu hút lượng fan hùng hậu trên cả nước. Người ta nhớ đến Yugi với những trận đấu bài chiến thuật (Hay bài ma thuật, bài Magic đòi hỏi tư duy, sự tính toán và trí tuệ của người chơi) đầy hấp dẫn và kịch tính, kéo theo là cơn sốt chơi bài ở ngoài đời thực, với hàng trăm triệu quân bài được bán ra trên toàn thế giới, cùng với những giải đấu quốc tế nhận được rất nhiều sự quan tâm của cộng đồng \"bài thủ\" ở các quốc gia. Trong lần trở lại này, \"huyền thoại\" Yu-Gi-0h! sẽ được chỉn chu lại về cả nội dung và hình thức để đáp ứng sự kì vọng của độc giả.', 100, 18000, NULL, 'Kazuki Takahashi', 'NXB Kim Đồng');
 
 -- --------------------------------------------------------
 
@@ -136,6 +238,15 @@ CREATE TABLE `cart` (
   `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cid`, `uid`, `bid`, `amount`) VALUES
+(2, 1, 1, 10),
+(3, 8, 1, 1),
+(4, 9, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -149,6 +260,15 @@ CREATE TABLE `review` (
   `rating` int(11) NOT NULL,
   `comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `review`
+--
+
+INSERT INTO `review` (`rid`, `uid`, `bid`, `rating`, `comment`) VALUES
+(1, 1, 1, 4, 'Truyện hay mà bìa bùi nhùi quá :))'),
+(3, 8, 1, 5, 'Truyện đáng đồng tiền bát gạo :))'),
+(4, 9, 1, 1, 'Tui đang đọc cái gì vậy? :))');
 
 -- --------------------------------------------------------
 
@@ -175,7 +295,8 @@ INSERT INTO `user` (`uid`, `username`, `password`, `name`, `phone`, `email`, `ro
 (1, 'hoanduy', '*3060F61EB5671C11A3605E847AF27763E555374A', 'Nguyễn Trần Hoàn Duy', '0378927810', 'duy@gmail.com', 'customer', 1),
 (8, 'hieule', '*8C869A400A96A09D7483D88A490701C56D474BF1', 'Hiếu Đẹp Trai', '0969969696', 'hieulengoc@outlook.com', 'customer', 1),
 (9, 'longpham', '*F55C3F419C97BDEEBAD764C2DFF8BE3B4990D9CE', 'Long Pham', '0378823356', 'longpham@gmail.com', 'customer', 1),
-(10, 'dungmeo', '*FFCB7EF476F95690D1FFB871DEA274343CD9C284', 'Dũng Đình Rumble', '0345123456', 'dungdinh@gmail.com', 'customer', 1);
+(10, 'dungmeo', '*FFCB7EF476F95690D1FFB871DEA274343CD9C284', 'Dũng Đình Rumble', '0345123456', 'dungdinh@gmail.com', 'customer', 1),
+(11, 'booki', '*962A88555204B46D42F282DCF09A9584CC9203B5', 'Booki', '0324192276', 'booki@gmail.com', 'admin', 1);
 
 --
 -- Indexes for dumped tables
@@ -220,25 +341,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
--- app/setup/proc.sql
+++ app/setup/proc.sql
@@ -223,7 +223,7 @@ CREATE OR REPLACE PROCEDURE `GET_BOOK`(
     IN `id` INT 
 ) 
 BEGIN 
-	SELECT name, amount, description, price
+	SELECT name, author, publisher, amount, description, price
     FROM `book` 
     WHERE bid = id; 
 END $$
@@ -237,7 +237,7 @@ DELIMITER ;
 DELIMITER $$
 CREATE OR REPLACE PROCEDURE `GET_ALL_BOOKS`()
 BEGIN 
-	SELECT name, amount, description, price
+	SELECT name, author, publisher, amount, description, price
     FROM `book`;
 END $$
 DELIMITER ;
@@ -249,6 +249,8 @@ DELIMITER $$
 CREATE OR REPLACE PROCEDURE `UPDATE_BOOK_INFO`(
     IN id INT,
     IN _name TEXT COLLATE utf8mb4_unicode_ci,
+    IN _author TEXT COLLATE utf8mb4_unicode_ci,
+    IN _publisher TEXT COLLATE utf8mb4_unicode_ci,
     IN _description TEXT COLLATE utf8mb4_unicode_ci,
     IN _image TEXT COLLATE utf8mb4_unicode_ci,
     IN _amount INT,
@@ -258,6 +260,8 @@ BEGIN
 	UPDATE `book`
     SET 
         name = _name,
+        author = _author,
+        publisher = _publisher,
         description = _description,
         image = _image,
         amount = _amount,
@@ -272,20 +276,173 @@ DELIMITER ;
 */
 DELIMITER $$
 CREATE OR REPLACE PROCEDURE `ADD_BOOK`(
-    IN _name TEXT COLLATE utf8mb4_unicode_ci,
-    IN _description TEXT COLLATE utf8mb4_unicode_ci,
-    IN _image TEXT COLLATE utf8mb4_unicode_ci,
-    IN _amount INT,
-    IN _price INT
+    IN `_name` TEXT,
+    IN `_author` TEXT,
+    IN `_publisher` TEXT,
+    IN `_description` TEXT,
+    IN `_image` TEXT,
+    IN `_amount` INT,
+    IN `_price` INT
 )
 BEGIN 
-	INSERT INTO `book` (name, description, image, amount, price)
+	INSERT INTO `book` (name, description, author, publisher, image, amount, price)
     VALUES(
-        name = _name,
-        description = _description,
-        image = _image,
-        amount = _amount,
-        price = _price
+        _name,
+        _description,
+        _author,
+        _publisher,
+        _image,
+        _amount,
+        _price
     );
+END $$
+DELIMITER ;
+
+/*
+	Procedure: REMOVE_BOOK
+    Params: _bid: book id
+*/
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE `REMOVE_BOOK`(
+    IN `_bid` INT
+)
+BEGIN 
+	DELETE FROM `book`
+    WHERE bid = _bid;
+END $$
+DELIMITER ;
+
+/**************
+REVIEWS
+**************/
+/*
+	PROCEDURE: ADD_REVIEW
+*/
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE `ADD_REVIEW`(
+    IN _uid INT,
+    IN _bid INT,
+    IN _rating INT,
+    IN _comment TEXT
+)
+BEGIN
+    INSERT INTO `review` (uid, bid, rating, comment)
+    VALUES (_uid, _bid, _rating, _comment);
+END $$
+DELIMITER ;
+
+/*
+	PROCEDURE: GET_BOOK_REVIEWS
+    Description: Get all reviews of the book by book id
+    Return: table(user.name, rating, comment) 
+*/
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE `GET_BOOK_REVIEWS`(
+    IN _bid INT
+)
+BEGIN 
+	SELECT user.name, rating, comment
+    FROM user, book, review
+    WHERE
+        review.bid = _bid
+        AND book.bid = review.bid
+        AND user.uid = review.uid;
+END $$
+DELIMITER ;
+
+/**************
+CARTS
+**************/
+
+/*
+	PROCEDURE: SHOW_CART_ITEMS
+    Description: Show user's cart using user id
+    Param
+        - _uid: user id
+    Return: table(book.name, amount, unit_price, total_price)
+*/
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE `SHOW_CART_ITEMS`(
+    IN _uid INT
+)
+BEGIN 
+	SELECT book.name, cart.amount, book.price, cart.amount*book.price as total_price
+    FROM `book`, `user`, `cart`
+    WHERE
+        cart.uid = _uid 
+        AND cart.bid = book.bid
+        AND cart.uid = user.uid;
+END $$
+DELIMITER ;
+
+/*
+	PROCEDURE: ADD_TO_CART
+    Description: Helper procedure when user click on 'Add to cart' button
+        - Book has been in the cart: increase the current amount of this book in the cart by 1
+        - Book hasn't been in the cart: add new cart item with amount = 1
+    Param
+        - _uid: user id
+        - _bid: book id
+*/
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE `ADD_TO_CART`(
+    IN _uid INT,
+    IN _bid INT
+)
+BEGIN 
+    IF(EXISTS(
+        SELECT uid 
+        FROM `cart`
+        WHERE uid = _uid AND bid = _bid
+    )) THEN
+        UPDATE `cart`
+        SET amount = amount + 1
+        WHERE uid = _uid AND bid = _bid;
+
+    ELSE 
+        INSERT INTO `cart`(uid, bid, amount)
+        VALUES(_uid, _bid, 1);
+    END IF;
+	
+END $$
+DELIMITER ;
+
+
+/*
+	PROCEDURE: UPDATE_CART_ITEM
+    Description: Update amount of book in cart
+    Param
+        - _uid: user id
+        - _bid: book id
+        - _amount: amount to update
+*/
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE `UPDATE_CART_ITEM`(
+    IN _uid INT,
+    IN _bid INT,
+    IN _amount INT
+)
+BEGIN 
+    UPDATE `cart`
+    SET amount = _amount
+    WHERE uid = _uid AND bid = _bid;
+END $$
+DELIMITER ;
+
+/*
+	PROCEDURE: REMOVE_CART_ITEM
+    Description: Remove book in cart
+    Param
+        - _uid: user id
+        - _bid: book id
+*/
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE `REMOVE_CART_ITEM`(
+    IN _uid INT,
+    IN _bid INT
+)
+BEGIN 
+    DELETE FROM `cart`
+    WHERE uid = _uid AND bid = _bid;
 END $$
 DELIMITER ;
